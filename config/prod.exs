219710123c82c7b3ced7f9d81368e51dde8d85bb
Pablo Dávila <pablo@aztlansoft.com>
2020-09-21 17:07:50 -0500

    removed static assets config in prod

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :delivery_location_service, DeliveryLocationServiceWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "vast-reaches-60349.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-#  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: ""
 
 # Do not print debug messages in production
 config :logger, level: :info
