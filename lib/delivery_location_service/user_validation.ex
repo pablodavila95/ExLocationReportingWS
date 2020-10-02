d5742ca51d47c3898ff4cb5785b48a1f0dbf2065
Pablo Dávila <pablo@aztlansoft.com>
2020-10-02 14:19:18 -0500

    added env var for api url

--- lib/delivery_location_service/user_validation.ex
+++ lib/delivery_location_service/user_validation.ex
@@ -1,9 +1,13 @@
 defmodule DeliveryLocationService.UserValidation do
-  defp backend_verify(), do: "https://bcf26522eae5.ngrok.io/verify/token"
+  defp backend_verify() do
+    api_url = System.get_env("API_URL") || "https://localhost:9000"
+    api_url <> "/verify/token"
+  end
 
   def validate(:admin, token) do
     roles = ["SUPER_ADMIN", "ADMIN_COMPANY"]
     %{"user_id" => user_id, "role_access" => role_access} = user_json(token)
+
     case Enum.member?(roles, role_access) do
       true -> {:ok, user_id}
       false -> {:error, "Role is not valid."}
@@ -13,6 +17,7 @@ defmodule DeliveryLocationService.UserValidation do
   def validate(:driver, token) do
     roles = ["DRIVER"]
     %{"user_id" => user_id, "role_access" => role_access} = user_json(token)
+
     case Enum.member?(roles, role_access) do
       true -> {:ok, user_id}
       false -> {:error, "Role is not valid."}
@@ -22,6 +27,7 @@ defmodule DeliveryLocationService.UserValidation do
   def validate(:restaurant, token) do
     roles = ["RESTAURANT"]
     %{"user_id" => user_id, "role_access" => role_access} = user_json(token)
+
     case Enum.member?(roles, role_access) do
       true -> {:ok, user_id}
       false -> {:error, "Role is not valid."}
@@ -39,12 +45,22 @@ defmodule DeliveryLocationService.UserValidation do
 
   defp extract(data) do
     # Roles: SUPER_ADMIN | ADMIN_COMPANY | RESTAURANT | DRIVER
-    if Map.get(data, "customerCompany") != nil do
-      %{"roleAccess" => role_access, "customerCompany" => %{"id" => user_id}} = data
-      %{"user_id" => user_id, "role_access" => role_access}
-    else
-      %{"id" => user_id, "roleAccess" => role_access} = data
-      %{"user_id" => user_id, "role_access" => role_access}
+    case Map.get(data, "customerCompany") do
+      nil ->
+        %{"roleAccess" => role_access, "customerCompany" => %{"id" => user_id}} = data
+        %{"user_id" => user_id, "role_access" => role_access}
+
+      _ ->
+        %{"id" => user_id, "roleAccess" => role_access} = data
+        %{"user_id" => user_id, "role_access" => role_access}
     end
+
+    # if Map.get(data, "customerCompany") != nil do
+    #   %{"roleAccess" => role_access, "customerCompany" => %{"id" => user_id}} = data
+    #   %{"user_id" => user_id, "role_access" => role_access}
+    # else
+    #   %{"id" => user_id, "roleAccess" => role_access} = data
+    #   %{"user_id" => user_id, "role_access" => role_access}
+    # end
   end
 end
