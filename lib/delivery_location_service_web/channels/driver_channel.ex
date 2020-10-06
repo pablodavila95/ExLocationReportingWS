ab3f27c309a97c2df3cbac485dd7194cd548e556
Pablo Dávila <pablo@aztlansoft.com>
2020-10-06 00:07:07 -0500

    added log

--- lib/delivery_location_service_web/channels/driver_channel.ex
+++ lib/delivery_location_service_web/channels/driver_channel.ex
@@ -15,6 +15,7 @@ defmodule DeliveryLocationServiceWeb.DriverChannel do
 
   def join("driver:" <> driver_id, %{"lat" => lat, "long" => long}, socket) do
     Logger.info(inspect(socket.assigns.driver_id))
+    Logger.info(inspect(driver_id))
 
     if socket.assigns.driver_id == driver_id do
       case LocationServer.location_data_pid(driver_id) do
