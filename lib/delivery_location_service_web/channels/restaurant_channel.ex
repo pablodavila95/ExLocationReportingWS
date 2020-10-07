f0fb740cfd2af15d579da4fb78fb6007d5e8414b
Pablo Dávila <pablo@aztlansoft.com>
2020-10-07 17:33:22 -0500

    added restaurant logs at join

--- lib/delivery_location_service_web/channels/restaurant_channel.ex
+++ lib/delivery_location_service_web/channels/restaurant_channel.ex
@@ -6,8 +6,12 @@ defmodule DeliveryLocationServiceWeb.RestaurantChannel do
   alias DeliveryLocationService.Location
   alias DeliveryLocationService.LocationsHelper
   alias DeliveryLocationServiceWeb.Endpoint
+  require Logger
 
   def join("restaurant:" <> restaurant_id, _params, socket) do
+    Logger.info("server said", inspect(Integer.to_string(socket.assigns.restaurant_id)))
+    Logger.info("user said", inspect(restaurant_id))
+
     if Integer.to_string(socket.assigns.restaurant_id) == restaurant_id do
       send(self(), {:after_join})
       {:ok, socket |> assign(:drivers, []) |> assign(:restaurant_id, restaurant_id)}
